3305f4374336bbeee142d8885713fcdedc624427
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2023-10-01 12:02:35 +0530

    first commit

---
+++ datatype.java
@@ -0,0 +1,37 @@
+public class datatype {
+    public static void main(String[] args) {
+        // int i = 10i;
+
+        // byte b = 10b;
+
+        // short s = 10s;
+
+        long l = 10l;
+
+        // char c = 'c'c;
+        // char che="sjbjh"// invalid cuz in double quotes only valid single quotes
+        // char ab='ab';
+        char h = 65;
+        char ch = 96;
+        char fg = 19998;
+        char gf = 19977;
+        System.out.println(h);
+        System.out.println(ch);
+        System.out.println(fg);
+        System.out.println(gf); // output of gf and fg will be question mark cuz that values are not present in system these  values can be seen on unicode.org site
+
+        // boolean bn = falseb;
+
+        float f = 123.45f;
+
+        double d = 123.456D;
+        //  double a = 0789;// invalid cuz no point(.) indirectly octal first bit is 0 so octal but it contains 8 so invalid
+        double t = 0786.0; 
+        double db = 0xface;// valid bcuz no point(.) to represent floating value so it will be represesnted as integral meand hexadecimal so valid
+        //  double j=0xface.0;//invalid as it becomes floating and floating can only contain decimal values
+    
+            // int x = 10.0;
+            // System.out.println(x);
+            
+    }
+}
---
+++ identifier.java
@@ -0,0 +1,23 @@
+class  identify{
+    public static void main(String[] args)
+    {
+        int String = 10;
+        int Runnable = 20;
+        System.out.println(String);
+        System.out.println(Runnable);
+    }
+}
+/* identifiers 
+ * total_123
+ * total#  invalid
+ * 123total invalid
+ * totql123
+ * cas$
+ * _$_$_$
+ * arr@gw invalid
+ * java3jh
+ * Integre
+ * Int
+ * int invalid
+ * 
+ */
