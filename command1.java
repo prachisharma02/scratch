1807eb259cf66b3e101af5eeff02395abaac11c8
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2023-10-06 23:33:26 +0530

    day6

---
+++ command1.java
@@ -0,0 +1,14 @@
+public class command1 {
+    
+    public static void main(String[] args) {
+        // for (int i = 0; i <args.length;i++){   // javac command1.java
+       //   System.out.println(args[i]);          // java command1 prachi khushi sharma
+//    }                                           // prachi
+                                                  // khushi
+                                                  // sharma
+System.out.println(args[0]); // as command line arguments seperate the strings on the basis of space 
+                             // and if we want to provide a single string but with space for example prachi sharma
+                             // as single argument then if we aree doing args[0i then it will give only prachi 
+                             //but if we want prachi sharma then put themn in double quotes
+    }
+}
---
+++ psvm4.java
@@ -0,0 +1,6 @@
+ class psvm5 {
+    static{
+        System.out.println("static block");
+    }
+}
+// error no main method but in 1.6 it will be executed the error
