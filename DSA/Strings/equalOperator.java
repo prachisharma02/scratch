21d37e12eb3d332c1da07b2ddc0219b73a5a8768
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2023-10-10 01:03:56 +0530

    day9

--- DSA/Strings/equalOperator.java
+++ DSA/Strings/equalOperator.java
@@ -1,20 +1,23 @@
 public class equalOperator {
     public static void main(String[] args) {
   
+        String a = "prachi";
+        String b = "prachi";
+        System.out.println(a==b); //true
         String s=new String("hello");
         String p = new String("hello");
 
         StringBuffer sb = new StringBuffer("hello");
         StringBuffer sb2 = new StringBuffer("hello");
 
-        System.out.println(s == p); //false
-        System.out.println(sb == sb2); //false
+        // System.out.println(s == p); //false
+        // System.out.println(sb == sb2); //false
 
-        String s2=s;
-        System.out.println(s == s2);//true
+        // String s2=s;
+        // System.out.println(s == s2);//true
         
-        StringBuffer sb3=sb2;
-        System.out.println(sb3 == sb2); //true
+        // StringBuffer sb3=sb2;
+        // System.out.println(sb3 == sb2); //true
         
 
     }
---
+++ basics/printstream.java
@@ -0,0 +1,10 @@
+public class printstream {
+    public static void main(String[] args) {
+        System.out.println(56);
+        System.out.println("prachi");
+        System.out.printf("hello");
+            //System.out.println(new);
+
+    }
+
+}
