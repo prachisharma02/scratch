9803d21bf2c3df15dce5806dcffffaa8dc28a7e9
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2023-10-14 16:52:48 +0530

    day14

---
+++ DSA/Strings/string.java
@@ -0,0 +1,8 @@
+class compare {
+    public static void main(String[] args) {
+        char[] a = { 'p', 'r', 'a', 'c', 'h', 'i' };
+        String s = "prachi";
+        System.out.println(s);
+        System.out.println(a);
+    }
+}
