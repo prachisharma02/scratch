b20e4283fe735cd84b398e861563811f3f9f9d82
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2023-10-20 20:06:00 +0530

    day16

--- basics/flowcontrol.java
+++ basics/flowcontrol.java
@@ -16,5 +16,12 @@ if (true)
     }
 //      if(true)  // invald
 //         int x = 10; // invalid cuz declarations of x is done but no use of x we cant use x furthur 
-//  }
+
+
+if(true)
+if(false)
+    System.out.println("hello");
+else // this else is related to nearest if means above this hello if
+System.out.println("hi");
+ }
 }
