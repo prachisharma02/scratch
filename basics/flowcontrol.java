fe0bfd4392965c3ee588e31deb436cc639b066a7
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2023-10-20 20:01:33 +0530

    day16

--- basics/flowcontrol.java
+++ basics/flowcontrol.java
@@ -7,6 +7,14 @@ public class flowcontrol {
 //      }
 //      else
 //      System.out.println(9);
-if(true)System.out.println("hello"); // valid
- }
+if (true)
+    System.out.println("hello"); // valid
+
+    if(true)
+    {
+        int x = 10;
+    }
+//      if(true)  // invald
+//         int x = 10; // invalid cuz declarations of x is done but no use of x we cant use x furthur 
+//  }
 }
