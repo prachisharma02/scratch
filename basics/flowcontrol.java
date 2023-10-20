713ebff3b8a3a18384d64d8b364a9887c4ee3bdf
Prachi Sharma <111728451+prachisharma02@users.noreply.github.com>
2023-10-20 19:22:23 +0530

    new commit

--- basics/flowcontrol.java
+++ basics/flowcontrol.java
@@ -1,5 +1,11 @@
 public class flowcontrol {
  public static void main(String[] args) {
-    
- }
+//      int x = 0;
+//      if (x) {                           in java boolean values are only true and false so x=0 is not boolean and flow 
+                                        // control needs boolean value to execute  so error
+//          System.out.println(6);
+//      }
+//      else
+//      System.out.println(9);
+//  }
 }
